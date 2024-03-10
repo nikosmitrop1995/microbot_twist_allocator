2f51d347b174acd821ded4c9450a9bbdad51fa19
nikosmitrop1995 <nikosmitrop1995@gmail.com>
2024-03-10 15:10:39 +0000

    feat: first draft of the C++ node

---
+++ src/microbot_twist_allocator_node.cpp
@@ -0,0 +1,24 @@
+#include <iostrean>
+#include <rclcpp.h>
+
+class TwistAllocator() : rclcpp::Node(){
+  private:
+    /* data */
+  public:
+    TwistAllocator(/* args */);
+    ~TwistAllocator();
+};
+  
+TwistAllocator::TwistAllocator(/* args */)
+{
+}
+
+TwistAllocator::~TwistAllocator()
+{
+}
+
+
+
+int main(){
+
+}
