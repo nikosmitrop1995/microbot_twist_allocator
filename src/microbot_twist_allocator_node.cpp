a57bfb2e3b119e0cc387b3358a15692b5a5cc4aa
nikosmitrop1995 <nikosmitrop1995@gmail.com>
2024-03-11 14:49:34 +0000

    feat: first version of node where it print the output of the cmd_vel topic

--- src/microbot_twist_allocator_node.cpp
+++ src/microbot_twist_allocator_node.cpp
@@ -1,24 +1,36 @@
-#include <iostrean>
-#include <rclcpp.h>
+#include <iostream>
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/twist.hpp>
 
-class TwistAllocator() : rclcpp::Node(){
-  private:
-    /* data */
+class TwistAllocator : public rclcpp::Node
+{
   public:
-    TwistAllocator(/* args */);
-    ~TwistAllocator();
+    TwistAllocator(): Node("microbot_twist_allocator_node")
+    {
+      RCLCPP_INFO(this->get_logger(), "Start microbot_twist_allocator_node");
+      subscription_ = this->create_subscription<geometry_msgs::msg::Twist>(
+        "/cmd_vel",10,std::bind(&TwistAllocator::twist_callback, this, std::placeholders::_1)
+      );
+    }
+    ~TwistAllocator()
+    {
+      RCLCPP_INFO(this->get_logger(), "BYE BYE!!!");
+    }
+    
+  private:
+    void twist_callback(const geometry_msgs::msg::Twist::SharedPtr msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "Linear Velocity on x-axis: %f", msg->linear.x);
+      RCLCPP_INFO(this->get_logger(), "Angular Velocity on yaw: %f", msg->angular.z);
+    }
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_;
+
 };
-  
-TwistAllocator::TwistAllocator(/* args */)
+
+int main(int argc, char * argv[])
 {
-}
-
-TwistAllocator::~TwistAllocator()
-{
-}
-
-
-
-int main(){
-
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<TwistAllocator>());
+  rclcpp::shutdown();
+  return 0;
 }
