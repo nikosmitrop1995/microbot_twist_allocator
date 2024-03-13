816608db4a60aa9d43930dbdc483697965f7bb59
nikosmitrop1995 <nikosmitrop1995@gmail.com>
2024-03-13 04:42:53 +0000

    feat: make the allocator node to publish left and right wheel commanded velocity

--- src/microbot_twist_allocator_node.cpp
+++ src/microbot_twist_allocator_node.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist.hpp>
+#include <std_msgs/msg/float32.hpp>
+
+#define R 0.0598
+#define L 0.109
 
 class TwistAllocator : public rclcpp::Node
 {
@@ -8,7 +12,13 @@ class TwistAllocator : public rclcpp::Node
     TwistAllocator(): Node("microbot_twist_allocator_node")
     {
       RCLCPP_INFO(this->get_logger(), "Start microbot_twist_allocator_node");
-      subscription_ = this->create_subscription<geometry_msgs::msg::Twist>(
+      
+      // Publishers
+      publisher_left_wheel_vel_ = this->create_publisher<std_msgs::msg::Float32>("left_wheel_vel",10);
+      publisher_right_wheel_vel_ = this->create_publisher<std_msgs::msg::Float32>("right_wheel_vel",10);
+      
+      // Subscriber
+      subscriber_ = this->create_subscription<geometry_msgs::msg::Twist>(
         "/cmd_vel",10,std::bind(&TwistAllocator::twist_callback, this, std::placeholders::_1)
       );
     }
@@ -20,20 +30,40 @@ class TwistAllocator : public rclcpp::Node
   private:
     void twist_callback(const geometry_msgs::msg::Twist::SharedPtr msg) const
     {
-      RCLCPP_INFO(this->get_logger(), "Linear Velocity on x-axis: %f", msg->linear.x);
-      RCLCPP_INFO(this->get_logger(), "Angular Velocity on yaw: %f", msg->angular.z);
-
+      publish_wheel_vel(msg->linear.x, msg->angular.z);
+    }
+    
+    void publish_wheel_vel(const float& ux, const float& w) const
+    {
       // Left wheel velocity
       // υL = (2 * ux + ω*L)/(2*R)
       // Right wheel velocity
       // υR = (2 * ux - ω*L)/(2*R)
-      // Where ux is linear.x
+      // ux is linear.x
       // ω is angular.z
       // L is the distance between the wheels
       // R is the radius of the wheel
-    }
-    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_;
+      
+      // Left wheel velocity
+      float uL = ((2 * ux) + (w * L)) / (2 * R);
+      // Right wheel velocity
+      float uR = ((2 * ux) - (w * L)) / (2 * R);
 
+      auto msg = std_msgs::msg::Float32();
+
+      // Assign and publish left wheel velocity
+      msg.data = uL;
+      publisher_left_wheel_vel_->publish(msg);
+
+      // Assign and publish right wheel velocity
+      msg.data = uR;
+      publisher_right_wheel_vel_->publish(msg);
+
+    }
+
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscriber_;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher_left_wheel_vel_;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher_right_wheel_vel_;
 };
 
 int main(int argc, char * argv[])
